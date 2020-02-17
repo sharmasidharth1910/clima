7138ffb84cdf3a49207a1199b5235b6eaac9de08
sharmasidharth1910 <sharmasidharth1910@gmail.com>
2020-02-17 22:29:43 +0530

    Initial commit includes the function to ask the user for the permission to access its location and function to detect the location on startup.

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
+  void getLocation() async
+  {
+    Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+    print(position);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
